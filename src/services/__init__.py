22428cb2df85233d14cb940ef84304bc2c80fbea
Priyanshu Kumar <priyanshu.dude273@gmail.com>
2025-10-31 09:37:26 +0530

    Changes made

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,7 +1,6 @@
 """Service layer for ClipScape."""
 
 from .ClipboardService import ClipboardService
-from .PeerNetworkService import PeerNetworkService
 from .RedisService import RedisService
 
-__all__ = ["ClipboardService", "PeerNetworkService", "RedisService"]
+__all__ = ["ClipboardService", "RedisService"]
